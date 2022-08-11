ed96d80f13bb35d7a379ca41e912013ce6478ad5
yin <yin@swifthealth.cn>
2022-08-11 19:52:30 +0800

    first commit

---
+++ src/plugin/pages/list/list.tsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { View } from '@tarojs/components'
+import ListItem from '../../components/listItem/listItem'
+import './list.scss'
+
+export default class Index extends Component {
+  state = {
+    list: [{
+      name: 'A',
+      value: '1'
+    }, {
+      name: 'B',
+      value: '2'
+    }, {
+      name: 'C',
+      value: '3'
+    }]
+  }
+
+  render () {
+    return (
+      <View>
+        {this.state.list.map(item => {
+          return <ListItem name={item.name} value={item.value} key={item.name} />
+        })}
+      </View>
+    )
+  }
+}
